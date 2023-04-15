7a5d557c6f1ddab77eb6a02b5e7b02fed5f7446a
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-04-15 12:17:42 -0400

    MVP about

--- rjc_react_port/src/components/pages/Home.js
+++ rjc_react_port/src/components/pages/Home.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import ak from '../../assets/me/ak.jpg';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+       <div className="d-flex justify-content-center align-items-center flex-column">
+      <img
+        src={ak}
+        className="my-2"
+        style={{ width: "25%", height: "25%" }}
+        alt="cover"
+      />
+      <h1>Robert J Clemmer</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      Robert grew up in Staunton and after college spent time working outdoors
+       with conservation programs in northern New Mexico, Arizona, and Lancaster, 
+       Pennsylvania. After adventuring in Alaska as a dog musher, Robert discovered 
+       a passion for farming as an Environmental Educator at the Rock Eagle 4H Center 
+       in Georgia, managing the farm at their History site. In 2018, Robert moved back 
+       home and is continuing his farming journey.
+
+      Robert has a degree in History and a degree in Classical Studies from Hampden Sydney College. 
+      He is also a Master Gardener and has a Blue Ribbon for his tomatoes. When he’s not on the farm, 
+      Robert is either playing mandolin, trail running, 3D printing, or spending time with his two dogs.
       </p>
+      
     </div>
   );
 }
