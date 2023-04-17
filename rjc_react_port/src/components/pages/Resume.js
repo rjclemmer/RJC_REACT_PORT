79ba7fbf97f79e70b55e4427269aed9615d1a579
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-04-17 18:23:15 -0400

    resume

--- rjc_react_port/src/components/pages/Resume.js
+++ rjc_react_port/src/components/pages/Resume.js
@@ -4,14 +4,10 @@ export default function Resume() {
   return (
     <div>
       <h1>My Resume</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <a href="https://github.com/rjclemmer/RJC_REACT_PORT/blob/main/rjc_react_port/src/assets/Resume%20RJC%20040823.pdf">
+      Click here to download my resume from my Github profile where it is
+      hosted!
+    </a>
     </div>
   );
 }
